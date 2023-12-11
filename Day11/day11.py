13524d44759b072d6534729f07d41cfa1421c223
David Xue <David.Xue37@myhunter.cuny.edu>
2023-12-11 16:33:31 -0500

    Day 11

---
+++ Day11/day11.py
@@ -0,0 +1,114 @@
+
+
+def sum_length_galaxies(file):
+    galaxy = []
+    gal = []
+    with open(file) as f:
+        for line in f:
+            galaxy.append(list(line.strip('\n')))
+    row = list(range(len(galaxy)))
+    col = list(range(len(galaxy[0])))
+    for i in range(len(galaxy)):
+        for j in range(len(galaxy[i])):
+            if(galaxy[i][j]=='#'):
+                if(i in row):
+                    row.remove(i)
+                if(j in col):
+                    col.remove(j)
+    empty_row = ['.'] * len(galaxy)
+    count = 0
+    for i in row:
+        galaxy.insert(i+count,empty_row.copy())
+        count +=1
+    for i in range(len(galaxy)):
+        count = 0
+        for j in col:
+            galaxy[i].insert(j+count,'.')
+            count +=1
+    for i in range(len(galaxy)):
+        for j in range(len(galaxy[i])):
+            if(galaxy[i][j]=='#'):
+                gal.append([i,j])
+    pair_gals = {}
+    total = 0
+    for g1 in gal:
+        for g2 in gal:
+            if(g1==g2 or (tuple(g1),tuple(g2)) in pair_gals or (tuple(g2),tuple(g1)) in pair_gals):
+                continue
+            total = total + (abs(g1[0]-g2[0]) + abs(g1[1] - g2[1]))
+            #total += get_shortest_dist(galaxy,g1,g2) #the long way 
+            pair_gals[(tuple(g1),tuple(g2))] = 1
+    return total
+
+
+def print_gal(arr):
+    for i in range(len(arr)):
+        print(arr[i])
+
+def get_shortest_dist(arr,cor1,cor2):
+    queue = [cor1]
+    visited = {}
+    steps = 0
+    while(len(queue)!=0):
+        curr_len = len(queue)
+        for i in range(curr_len):
+            row = queue[i][0]
+            col = queue[i][1]
+            if(row==cor2[0] and col==cor2[1]):
+                return steps
+            if((row,col) in visited):
+                continue
+            visited[(row,col)] = 1
+            if(row+1<=len(arr)-1):
+                queue.append([row+1,col])
+            if(col-1>=0):
+                queue.append([row,col-1])
+            if(col+1<=len(arr[0])-1):
+                queue.append([row,col+1])
+        steps +=1
+        for i in range(curr_len):
+            queue.pop(0)
+
+
+def sum_length_million_galaxies(file):
+    galaxy = []
+    gal = []
+    with open(file) as f:
+        for line in f:
+            galaxy.append(list(line.strip('\n')))
+    row = list(range(len(galaxy)))
+    col = list(range(len(galaxy[0])))
+    for i in range(len(galaxy)):
+        for j in range(len(galaxy[i])):
+            if(galaxy[i][j]=='#'):
+                if(i in row):
+                    row.remove(i)
+                if(j in col):
+                    col.remove(j)
+    for i in range(len(galaxy)):
+        for j in range(len(galaxy[i])):
+            if(galaxy[i][j]=='#'):
+                curr_row = i
+                curr_col = j
+                for r in row:
+                    count = 0
+                    if(i>r):
+                        curr_row += 999999
+                for c in col:
+                    if(j>c):
+                        curr_col += 999999
+                gal.append([curr_row,curr_col])
+    pair_gals = {}
+    total = 0
+    for g1 in gal:
+        for g2 in gal:
+            if(g1==g2 or (tuple(g1),tuple(g2)) in pair_gals or (tuple(g2),tuple(g1)) in pair_gals):
+                continue
+            total = total + (abs(g1[0]-g2[0]) + abs(g1[1] - g2[1]))
+            pair_gals[(tuple(g1),tuple(g2))] = 1
+    return total
+
+
+
+print(sum_length_galaxies("input.txt"))
+print(sum_length_million_galaxies("input.txt"))
